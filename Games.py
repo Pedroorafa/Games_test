52d26f8c08778cb2d3614b084a16f19e815ffb6d
Pedro Rafael <pedroorafav@gmail.com>
2023-04-13 16:11:58 -0300

    04/13

--- Games.py
+++ Games.py
@@ -4,14 +4,26 @@ pygame.init()
 
 width = 500
 height = 500
-cor_branca = (255, 255, 255)
+color_white = (255, 255, 255)
+color_gray = (127, 127,127)
+size_surface = (32, 32)
 
 screnn = pygame.display.set_mode((width, height))
+rectangle = pygame.Surface(size_surface)
+rectangle2 = pygame.Surface(size_surface)
+rocket = pygame.image.load('./imagens/Rocket.png').convert_alpha()
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    screnn.fill(cor_branca)
+    screnn.fill(color_gray)
+    rectangle.fill('red')
+    rectangle2.fill('blue')
+    screnn.blit(rectangle, (50, 50))
+    screnn.blit(rectangle2, (60, 60))
+    screnn.blit(rocket, (100, 100))
+
     pygame.display.update()
